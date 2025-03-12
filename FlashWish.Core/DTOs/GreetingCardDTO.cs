0ca3d16a32a719986c0c03eecdd53081ae98dc0d
T-H-10 <t0534198344@gmail.com>
2025-03-13 01:33:12 +0200

    add dtos

---
+++ FlashWish.Core/DTOs/GreetingCardDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashWish.Core.DTOs
+{
+    public class GreetingCardDTO
+    {
+        public int CardID { get; set; }
+        public int userID { get; set; }
+        public int TemplateID { get; set; }
+        public int TextID { get; set; }
+        public int CategoryID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ FlashWish.Core/DTOs/TemplateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashWish.Core.DTOs
+{
+    public class TemplateDTO
+    {
+        public int TemplateID { get; set; }
+        public string TemplateName { get; set; }
+        public string TemplateDescription { get; set; }
+        public int CategoryID { get; set; }
+        public int UserID { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
