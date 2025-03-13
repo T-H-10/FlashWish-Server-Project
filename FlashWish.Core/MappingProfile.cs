cb880016876ae083c9589bf9d01b367f679da38e
T-H-10 <t0534198344@gmail.com>
2025-03-13 03:08:46 +0200

    add MappingProfile

---
+++ FlashWish.Core/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FlashWish.Core.DTOs;
+using FlashWish.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashWish.Core
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<Template, TemplateDTO>().ReverseMap();
+            CreateMap<GreetingCard, GreetingCardDTO>().ReverseMap();
+            CreateMap<GreetingMessage, GreetingMessageDTO>().ReverseMap();
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}
