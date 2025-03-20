08d870336947d025260ea65f2982a6a75aff0552
T-H-10 <t0534198344@gmail.com>
2025-03-20 11:29:51 +0200

    change the connection string

--- FlashWish.Data/DataContext.cs
+++ FlashWish.Data/DataContext.cs
@@ -18,8 +18,11 @@ namespace FlashWish.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=FlashWish1_db;TrustServerCertificate=True;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"postgresql://flashwish_user:6vrenHaV774iUv9gRjPIBlVH8zKkNXb5@dpg-cvdttbfnoe9s73eikm10-a:5432/flashwish_db");
             //base.OnConfiguring(optionsBuilder);//----
+
+            //local connenction string:
+            //"Server=(localdb)\MSSQLLocalDB;Database=FlashWish1_db;TrustServerCertificate=True;Trusted_Connection=True;"
         }
     }
 }
